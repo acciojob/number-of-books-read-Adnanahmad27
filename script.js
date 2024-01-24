bc6f90f255de38ff02fc49a2210586437afb9db5
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-24 17:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	library.forEach( (book) => {
+		if(book.readingStatus) count++;
+	});
+	return count;
 };
 
 // Do not change the code below
